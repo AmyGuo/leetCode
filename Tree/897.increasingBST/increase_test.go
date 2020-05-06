18ad553875eb819da39431b6e7d4629ad29fdea1
AmyGuo319 <353289125@qq.com>
2020-05-06 18:30:46 +0800

    add

---
+++ Tree/897.increasingBST/increase_test.go
@@ -0,0 +1,27 @@
+package _97_increasingBST
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIncreasingBST(t *testing.T) {
+	node := new(TreeNode)
+	node.Val = 5
+	node.Left = &TreeNode{Val: 3, Right: &TreeNode{Val: 4}, Left: &TreeNode{Val: 2, Left: &TreeNode{Val: 1}}}
+	node.Right = &TreeNode{Val: 6, Right: &TreeNode{Val: 8, Left: &TreeNode{Val: 7}, Right: &TreeNode{Val: 9}}}
+	IncreasingBST(node).PreOrder()
+	IncreasingBST2(node).PreOrder()
+}
+
+func (node *TreeNode) PreOrder() {
+	if node == nil {
+		return
+	}
+	node.Print()
+	node.Left.PreOrder()
+	node.Right.PreOrder()
+}
+func (node *TreeNode) Print() {
+	fmt.Print(node.Val, " ")
+}
