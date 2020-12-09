949db43abdf0a73fa4e5b508a944a72793188e65
AmyGuo <353289125@qq.com>
2020-12-09 10:45:28 +0800

    add greedy

--- greedy/eraseOverlapIntervals_test.go
+++ greedy/eraseOverlapIntervals_test.go
@@ -35,13 +35,38 @@ import (
 解释: 你不需要移除任何区间，因为它们已经是无重叠的了。
 */
 
+//先找出最多不重叠的数量，用数组长度减去不重叠的数量，就是需要移除的数量
 func eraseOverlapIntervals(intervals [][]int) int {
+	if len(intervals) == 0 {
+		return 0
+	}
 
+	for i := 0; i < len(intervals); i++ {
+		for j := i; j < len(intervals); j++ {
+			if intervals[i][1] > intervals[j][1] {
+				intervals[i], intervals[j] = intervals[j], intervals[i]
+			}
+		}
+	}
+	count := 1 //不重叠数量,至少有1个不重叠
+	x_end := intervals[0][1]
+	for i := 0; i < len(intervals); i++ {
+		if intervals[i][0] >= x_end {
+			count++
+			x_end = intervals[i][1]
+		}
+	}
+	return len(intervals) - count
 }
 
 func ExampleEraseOverlapIntervals() {
-	fmt.Println(eraseOverlapIntervals([][]int{{}, {}}))
-	fmt.Println(eraseOverlapIntervals())
-	fmt.Println(eraseOverlapIntervals())
-Output:
+	fmt.Println(eraseOverlapIntervals([][]int{{1, 2}, {2, 3}, {3, 4}, {1, 3}}))
+	fmt.Println(eraseOverlapIntervals([][]int{{1, 2}, {1, 2}, {1, 2}}))
+	fmt.Println(eraseOverlapIntervals([][]int{{1, 2}, {2, 3}}))
+	fmt.Println(eraseOverlapIntervals([][]int{{1, 100}, {11, 22}, {1, 11}, {2, 12}}))
+	//Output:
+	//1
+	//2
+	//0
+	//2
 }
